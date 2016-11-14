bb63d06f0189f1ad0acc1ca24e0a36d0d44ec4d8
sean-spatrick <sean.patrick@stonybrook.edu>
2016-11-14 18:03:06 -0500

    all changes
    massive javascript functionality

---
+++ JS/wolfie-main.js
@@ -0,0 +1,43 @@
+//Carousel Slider
+jQuery(document).ready(function($) {
+			$('.my-slider').unslider();
+		});
+//Navbar minimizer
+function init() {
+        window.addEventListener('scroll', function(e){
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                shrinkOn = 300,
+                header = document.querySelector("header");
+            if (distanceY > shrinkOn) {
+                classie.add(header,"smaller");
+            } else {
+                if (classie.has(header,"smaller")) {
+                    classie.remove(header,"smaller");
+                }
+            }
+        });
+    }
+    window.onload = init();
+
+//card expand
+
+//function init2(){
+  //  var click;
+    //window.addEventListner(onclick){
+      //  click = !click;
+        //if(click == true){
+          //  document.getElementById("FPro")style.width = "";
+        //}else{
+        //    if(click == false){
+        //    document.getElementById("FPro")style.    
+       //     }
+     //   }
+  //  }
+//}
+
+//function expandCard(){
+    document.getElementById("FPro").style.width = "100%";
+}
+//function condenseCard(){
+    document.getElementById("FPro").style.width = "0%";
+}
